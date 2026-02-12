0d95cf283b24e7b7609fc6f0360bfdfa9990d5b2
jovanyrh2008-hub <jovanyrh2008@gmail.com>
2026-02-11 16:26:04 -0800

    datos

---
+++ sesion_02.py
@@ -0,0 +1,19 @@
+# Tuplas 
+mi_tupla = (2, 4)
+print("Mi tupla: ",mi_tupla)
+
+# Lista 
+mi_lista = [1, 3.1416, "Jovany", mi_lista]
+print("El primer elemento de mi lista", mi_lista[0])
+print("El cuarto elemento de mi lista", mi_lista[3])
+print("El tercer  elemento de mi lista", mi_lista[2])
+
+# Diccionarios 
+mi_diccionario = {"mi_lista": mi_lista,
+                  "nombre": "Jovany",
+                  "Pi": 3.1416,
+                  "Tel": "664-4835491"}
+
+print("Llave para accesar a mi diccionario de mi_lista", mi_diccionario["mi lista"])
+print("Llave para accesar a mi diccionario de pi", mi_diccionario["pi"])
+print("Llave para accesar a mi diccionario de tel", mi_diccionario["tel"])
