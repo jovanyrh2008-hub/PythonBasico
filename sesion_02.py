326cc5bf15625928981a3bab0fb729cadfe12cad
jovanyrh2008-hub <jovanyrh2008@gmail.com>
2026-02-12 16:55:19 -0800

    los loops

--- sesion_02.py
+++ sesion_02.py
@@ -1,12 +1,12 @@
-# Tuplas 
-mi_tupla = (2, 4)
-print("Mi tupla: ",mi_tupla)
+# tuplas
+mi_tupla = (2,4)
+print("mi tupla: ", mi_tupla)
 
-# Lista 
-mi_lista = [1, 3.1416, "Jovany", mi_lista]
-print("El primer elemento de mi lista", mi_lista[0])
-print("El cuarto elemento de mi lista", mi_lista[3])
-print("El tercer  elemento de mi lista", mi_lista[2])
+# lsita
+mi_lista = [1, 3.1416, "juan", mi_tupla]
+print("el primer elento de mi lista:", mi_lista[0])
+print("el cuarto elento de mi lista:", mi_lista[3])
+print("el tercer elento de mi lista:", mi_lista[2])
 
 # Diccionarios 
 mi_diccionario = {"mi_lista": mi_lista,
@@ -14,6 +14,6 @@ mi_diccionario = {"mi_lista": mi_lista,
                   "Pi": 3.1416,
                   "Tel": "664-4835491"}
 
-print("Llave para accesar a mi diccionario de mi_lista", mi_diccionario["mi lista"])
-print("Llave para accesar a mi diccionario de pi", mi_diccionario["pi"])
-print("Llave para accesar a mi diccionario de tel", mi_diccionario["tel"])
+print("llave para acceder a mi diccionario  mi_lista",mi_diccionario["mi_lista"])
+print("llave para acceder a mi diccionario pi", mi_diccionario["pi"])
+print("llave para acceder a mi diccionario  tel", mi_diccionario["tel"])
