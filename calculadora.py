de118d3e4c8831b1a6023d93b6b9e00fabe019da
jovanyrh2008-hub <jovanyrh2008@gmail.com>
2026-02-26 17:14:39 -0800

    calculadora

---
+++ calculadora.py
@@ -0,0 +1,24 @@
+print("======== Mi Super Calculadora ==========")
+
+num_1 = float(input("Escriba el valor del primer numero: "))
+num_2 = float(input("Escriba el valor del segundo numero: "))
+operacion = input("¿Cual operacion deseas hacer? +, -, *, / -> ")
+
+def calculadora(num_1, num_2, operacion):
+    if operacion == "+":
+        return num_1 + num_2
+    elif operacion == "-":
+        return num_1 - num_2
+    elif operacion == "*":
+        return num_1 * num_2
+    elif operacion == "/":
+        if num_2 != 0:
+            return num_1 / num_2
+        else:
+            return "Error: No se puede dividir entre cero."
+    else:
+        return "Operación no válida."
+
+resultado = calculadora(num_1, num_2, operacion)
+
+print("El resultado es:", resultado)
